c37f72586d895d16c1800263d8df1aad2a9f27b7
dshamis317 <dshamis317@gmail.com>
2014-07-11 14:14:57 -0400

    created seed data for feed urls and methods for seeding db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,48 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-topics = ["Politics", "Entertainment", "Technology", "Business", "Sports"]
+topics = ["Politics", "Entertainment", "Technology", "Finance", "Sports"]
 
 topics.each do |topic|
   Topic.create({name: topic})
+end
+
+CSV.foreach("db/seed_data/politics.csv", :headers => true, :col_sep => ',', :encoding => 'windows-1251:utf-8') do |row|
+  Feed.create({
+    :topic_id => 1,
+    :name => row[0],
+    :url => row[1]
+    })
+end
+
+CSV.foreach("db/seed_data/entertainment.csv", :headers => true, :col_sep => ',', :encoding => 'windows-1251:utf-8') do |row|
+  Feed.create({
+    :topic_id => 2,
+    :name => row[0],
+    :url => row[1]
+    })
+end
+
+CSV.foreach("db/seed_data/technology.csv", :headers => true, :col_sep => ',', :encoding => 'windows-1251:utf-8') do |row|
+  Feed.create({
+    :topic_id => 3,
+    :name => row[0],
+    :url => row[1]
+    })
+end
+
+CSV.foreach("db/seed_data/finance.csv", :headers => true, :col_sep => ',', :encoding => 'windows-1251:utf-8') do |row|
+  Feed.create({
+    :topic_id => 4,
+    :name => row[0],
+    :url => row[1]
+    })
+end
+
+CSV.foreach("db/seed_data/sports.csv", :headers => true, :col_sep => ',', :encoding => 'windows-1251:utf-8') do |row|
+  Feed.create({
+    :topic_id => 5,
+    :name => row[0],
+    :url => row[1]
+    })
 end
