a935c216ca0b79803e478f05341c7f602ff3c789
dshamis317 <dshamis317@gmail.com>
2014-07-11 16:48:52 -0400

    refactoring of search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,7 +6,7 @@ class SearchController < ApplicationController
   def search
     search = params[:search]
     topic = params[:topics].to_i
-    db_feeds = Feed.where("topic_id = #{topic}")
+    db_feeds = Feed.where("topic_id = ?", topic)
     urls = []
     db_feeds.each do |feed|
       urls << feed.url
@@ -16,26 +16,26 @@ class SearchController < ApplicationController
     search_results = []
     keys.each do |key|
       source = {}
-      source[:title] = feeds[key].title
+      source[:title] ||= feeds[key].title
       source[:description] = feeds[key].description
       source[:site_url] = feeds[key].feed_url
       source[:modified] = feeds[key].last_modified
-      source[:stories] = {}
+      source[:stories] = []
       feeds[key].entries.each do |feed|
         if feed.title.downcase.include?(search.downcase)
           data = HTTParty.get("http://access.alchemyapi.com/calls/url/URLGetTextSentiment?apikey=e0f145265967a4aa21013b7107bf8a834dc9ffa2&url=#{feed.url}&outputMode=json&showSourceText=1")
           if data['text']
-            source[:stories][:title] = feed.title
-            source[:stories][:story_url] = feed.url
-            source[:stories][:sentiment_type] = data['docSentiment']['type']
-            source[:stories][:sentiment_score] =data['docSentiment']['score']
-            search_results << source
+            story = {}
+            story[:title] = feed.title
+            story[:story_url] = feed.url
+            story[:sentiment_type] = data['docSentiment']['type']
+            story[:sentiment_score] =data['docSentiment']['score']
+            source[:stories] << story
           end
         end
       end
+      search_results << source
     end
-    search_results = search_results.uniq
-    binding.pry
     redirect_to root_path
   end
 end
