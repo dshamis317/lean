45a88f1f21f1352d043213611e972790110ae325
dshamis317 <dshamis317@gmail.com>
2014-07-11 17:24:02 -0400

    refactored search#search and created two methods in search model

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,36 +6,8 @@ class SearchController < ApplicationController
   def search
     search = params[:search]
     topic = params[:topics].to_i
-    db_feeds = Feed.where("topic_id = ?", topic)
-    urls = []
-    db_feeds.each do |feed|
-      urls << feed.url
-    end
-    feeds = Feedjira::Feed.fetch_and_parse urls
-    keys = feeds.keys
-    search_results = []
-    keys.each do |key|
-      source = {}
-      source[:title] ||= feeds[key].title
-      source[:description] = feeds[key].description
-      source[:site_url] = feeds[key].feed_url
-      source[:modified] = feeds[key].last_modified
-      source[:stories] = []
-      feeds[key].entries.each do |feed|
-        if feed.title.downcase.include?(search.downcase)
-          data = HTTParty.get("http://access.alchemyapi.com/calls/url/URLGetTextSentiment?apikey=e0f145265967a4aa21013b7107bf8a834dc9ffa2&url=#{feed.url}&outputMode=json&showSourceText=1")
-          if data['text']
-            story = {}
-            story[:title] = feed.title
-            story[:story_url] = feed.url
-            story[:sentiment_type] = data['docSentiment']['type']
-            story[:sentiment_score] =data['docSentiment']['score']
-            source[:stories] << story
-          end
-        end
-      end
-      search_results << source
-    end
-    redirect_to root_path
+    urls = Feed.get_feed_urls(topic)
+    search_results = Feed.create_news_source_objects(urls, search)
+    render :json => search_results.to_json
   end
 end
--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,8 +1,43 @@
 class Feed < ActiveRecord::Base
   belongs_to :topic
 
-  def get_feed_urls(id)
+  def self.get_feed_urls(id)
+    db_feeds = Feed.where("topic_id = ?", id)
+    urls = []
+    db_feeds.each do |feed|
+      urls << feed.url
+    end
+    return urls
+  end
 
+  def self.create_news_source_objects(array, search_term)
+    search = search_term
+    feeds = Feedjira::Feed.fetch_and_parse array
+    keys = feeds.keys
+    search_results = []
+    keys.each do |key|
+      source = {}
+      source[:title] ||= feeds[key].title
+      source[:description] = feeds[key].description
+      source[:site_url] = feeds[key].feed_url
+      source[:modified] = feeds[key].last_modified
+      source[:stories] = []
+      feeds[key].entries.each do |feed|
+        if feed.title.downcase.include?(search.downcase)
+          data = HTTParty.get("http://access.alchemyapi.com/calls/url/URLGetTextSentiment?apikey=e0f145265967a4aa21013b7107bf8a834dc9ffa2&url=#{feed.url}&outputMode=json&showSourceText=1")
+          if data['text']
+            story = {}
+            story[:title] = feed.title
+            story[:story_url] = feed.url
+            story[:sentiment_type] = data['docSentiment']['type']
+            story[:sentiment_score] =data['docSentiment']['score']
+            source[:stories] << story
+          end
+        end
+      end
+      search_results << source
+    end
+    return search_results
   end
 
 end
