1e28fd5286f297a68184cd4eab1add84146195f9
dshamis317 <dshamis317@gmail.com>
2014-07-12 11:07:01 -0400

    addressed empty articles by adding .present? to 'if data['text']'

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -25,12 +25,12 @@ class Feed < ActiveRecord::Base
       feeds[key].entries.each do |feed|
         if feed.title.downcase.include?(search.downcase)
           data = HTTParty.get("http://access.alchemyapi.com/calls/url/URLGetTextSentiment?apikey=e0f145265967a4aa21013b7107bf8a834dc9ffa2&url=#{feed.url}&outputMode=json&showSourceText=1")
-          if data['text']
+          if data['text'].present?
             story = {}
             story[:title] = feed.title
             story[:story_url] = feed.url
             story[:sentiment_type] = data['docSentiment']['type']
-            story[:sentiment_score] =data['docSentiment']['score']
+            story[:sentiment_score] = data['docSentiment']['score']
             source[:stories] << story
           end
         end
