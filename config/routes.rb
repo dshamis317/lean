8ddb83e153b2b7db7e0de66d0402d3a18122e063
dshamis317 <dshamis317@gmail.com>
2014-07-12 13:24:34 -0400

    create routes for user creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   get '/' => 'search#index', as: 'root'
   post '/' => 'search#search'
+
+  get '/login' => 'sessions#new', as: 'login'
+  post '/sessions' => 'sessions#create', as: 'sessions'
+  delete '/logout' => 'sessions#destroy', as: 'logout'
+
+  get '/signup' => 'users#new', as: 'signup'
+  post '/users' => 'users#create', as: 'users'
+  get '/profile' => 'users#profile', as: 'profile'
 end
