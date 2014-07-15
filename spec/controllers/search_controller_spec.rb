6a8fb839866f1289bdd6e3b9b33326b1d0c53955
dshamis317 <dshamis317@gmail.com>
2014-07-15 18:42:55 -0400

    testing search_controller

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
 describe SearchController do
+  describe 'given topic' do
 
+    before :each do
+      @sports = Topic.new
+      @sports.name = 'Sports'
+      @sports.save
+    end
+
+    describe 'GET index' do
+
+      before :each do
+        get :index
+      end
+
+      it 'responds successfully' do
+        actual = response.code
+        expected = '200'
+        expect(actual).to eq(expected)
+      end
+
+      it 'assigns @topics' do
+        actual = assigns(:topics)
+        expected = [@sports]
+        expect(actual).to eq(expected)
+      end
+
+    end # GET index
+  end
 end
