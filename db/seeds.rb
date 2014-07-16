6720de5bf69fde9fc1d77ca31ed1288f6ae26cb3
dshamis317 <dshamis317@gmail.com>
2014-07-16 17:08:46 -0400

    fix seeds file for the sake of sanity

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,44 +9,49 @@
 topics = ["Politics", "Entertainment", "Technology", "Finance", "Sports"]
 
 topics.each do |topic|
-  Topic.create({name: topic})
+  Topic.find_or_create_by(name: topic)
 end
 
 CSV.foreach("db/seed_data/politics.csv", :headers => true, :col_sep => ',', :encoding => 'windows-1251:utf-8') do |row|
+  politics = Topic.find_by(name: 'Politics')
   Feed.create({
-    :topic_id => 1,
+    :topic_id => politics.id,
     :name => row[0],
     :url => row[1]
     })
 end
 
 CSV.foreach("db/seed_data/entertainment.csv", :headers => true, :col_sep => ',', :encoding => 'windows-1251:utf-8') do |row|
+  entertainment = Topic.find_by(name: 'Entertainment')
   Feed.create({
-    :topic_id => 2,
+    :topic_id => entertainment.id,
     :name => row[0],
     :url => row[1]
     })
 end
 
 CSV.foreach("db/seed_data/technology.csv", :headers => true, :col_sep => ',', :encoding => 'windows-1251:utf-8') do |row|
+  technology = Topic.find_by(name: 'Technology')
   Feed.create({
-    :topic_id => 3,
+    :topic_id => technology.id,
     :name => row[0],
     :url => row[1]
     })
 end
 
 CSV.foreach("db/seed_data/finance.csv", :headers => true, :col_sep => ',', :encoding => 'windows-1251:utf-8') do |row|
+  finance = Topic.find_by(name: 'Finance')
   Feed.create({
-    :topic_id => 4,
+    :topic_id => finance.id,
     :name => row[0],
     :url => row[1]
     })
 end
 
 CSV.foreach("db/seed_data/sports.csv", :headers => true, :col_sep => ',', :encoding => 'windows-1251:utf-8') do |row|
+  sports = Topic.find_by(name: 'Sports')
   Feed.create({
-    :topic_id => 5,
+    :topic_id => sports.id,
     :name => row[0],
     :url => row[1]
     })
